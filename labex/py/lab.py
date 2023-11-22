d08b737c8b2b870e23a111f363e0ddb84ebd9c81
boku <zaregototsukai13@gmail.com>
2023-11-22 22:46:27 +0530

    lab exam >.<

---
+++ labex/py/lab.py
@@ -0,0 +1,134 @@
+class Node():
+    def __init__(self, value):
+        self.parent = None
+        self.value = value
+        self.children = []
+        
+class Tree():
+    def __init__(self, root_val):
+        self.root = Node(root_val)
+        self.nodes = {}
+        self.nnodes = {}
+        
+    def create_tree(self, edges):
+        self.nodes[self.root.value] = self.root
+        for edge in edges:
+            # vertex_0 = 
+            vertex_1 = Node(edge[1])
+            if  edge[0] not in self.nodes:
+                self.nodes[edge[0]] = Node(edge[0])
+            self.nodes[edge[0]].children.append(vertex_1)
+            if edge[1] not in self.nodes:
+                self.nodes[edge[1]] = vertex_1
+            # if vertex_1 not in self.nodes:
+            #     self.nodes[vertex_1] = []
+            # self.nodes[vertex_1].append(vertex_0)
+            # if vertex_0 not in vertex_1.children:
+            #     vertex_1.children.append(vertex_0)
+                
+    def print_tree(self):
+        print(self.nodes)
+        for _, node in self.nodes.items():
+            for child in node.children:
+                print(node)
+                print(f"Node : {node.value}, Children : {child.value}")
+
+        
+        
+    
+    def bfs(self):
+        queue = []
+        visited = {}
+        leaf = []
+        all = []
+        res = []
+        
+        queue.append(self.root)
+        visited[self.root.value] = True
+          
+        # print("BFS_____________________")
+        while len(queue) != 0:
+            
+            for _, i in self.nodes.items():
+                visited[i.value] = 0
+            
+            v = queue.pop(0)
+            visited[v] = 1
+            # print("For thisss:", v)  
+            # print(v.children)          
+            for child in v.children:
+                if not visited[child.value]:    
+                    queue.append(child)
+               
+            all.append(v)     
+            if len(v.children) == 0:
+                leaf.append(v)
+            
+        return all, leaf
+    
+        
+    def bfs_after(self):
+        queue = []
+        visited = {}
+        leaf = []
+        all = []
+        res = []
+        
+        queue.append(self.root)
+        visited[self.root.value] = True
+          
+        # print("BFS_____________________")
+        while len(queue) != 0:
+            
+            for _, i in self.nnodes.items():
+                visited[i.value] = 0
+            
+            v = queue.pop(0)
+            visited[v] = 1
+            # print("For thisss:", v)  
+            # print(v.children)          
+            for child in v.children:
+                if not visited[child.value]:    
+                    queue.append(child)
+               
+            all.append(v)     
+            if len(v.children) == 0:
+                leaf.append(v)
+            
+        return all, leaf
+
+    def remove(self, leaf):
+        for l in leaf:
+            del self.nodes[l.value]
+        self.nnodes = self.nodes
+    
+
+number_of_nodes = int(input())
+root_val = input()
+# root_val = 'A'
+tree = Tree(root_val=root_val)
+
+number_of_edges = int(input())
+edge_list = []
+for i in range(number_of_edges):
+    s = input()
+    edge = s.strip().split()
+    edge_list.append(edge)
+
+# edge_list = [('A','B'), ('A','C'), ('A','D'), ('A', 'E'),
+#              ('B', 'F'), ('B', 'G'), ('B', 'H'), ('C', 'I'), ('C', 'J'), ('D', 'K'), ('G', 'L'), ('G', 'M'), ('I', 'N'), ('I', 'O'),
+#              ('K', 'P'), ('K', 'Q'), ('Q', 'R')]
+
+tree.create_tree(edge_list) 
+# tree.print_tree()           
+
+all, leaf = tree.bfs()
+
+# print("BFS")
+print(",".join([a.value for a in leaf]))
+
+tree.remove(leaf)
+# print("aaa")
+leaf_new = tree.bfs()
+print(",".join([a.value for a in leaf_new]))
+
---
+++ labex/py/what.py
@@ -0,0 +1,81 @@
+class Node():
+    def __init__(self, value):
+        self.parent = None
+        self.value = value
+        self.children = []
+        
+class Tree():
+    def __init__(self, root_val):
+        self.root = Node(root_val)
+        self.nodes = {}
+        self.nnodes = {}
+        
+    def create_tree(self, edges):
+        self.nodes[self.root.value] = self.root
+        for edge in edges:
+            vertex_1 = Node(edge[1])
+            if edge[0] not in self.nodes:
+                self.nodes[edge[0]] = Node(edge[0])
+            self.nodes[edge[0]].children.append(vertex_1)
+            if edge[1] not in self.nodes:
+                self.nodes[edge[1]] = vertex_1
+                
+    def print_tree(self):
+        print(self.nodes)
+        for _, node in self.nodes.items():
+            for child in node.children:
+                print(f"Node : {node.value}, Children : {child.value}")
+
+    def bfs(self):
+        queue = []
+        visited = {}
+        leaf = []
+        all_nodes = []
+        
+        queue.append(self.root)
+        visited[self.root.value] = True
+          
+        while len(queue) != 0:
+            v = queue.pop(0)
+            visited[v.value] = 1
+            for child in v.children:
+                if not visited[child.value]:    
+                    queue.append(child)
+            all_nodes.append(v)     
+            if len(v.children) == 0:
+                leaf.append(v)
+            
+        return all_nodes, leaf
+    
+    def remove(self, leaf):
+        for l in leaf:
+            del self.nodes[l.value]
+            p = l.parent       
+
+# number_of_nodes = int(input())
+# root_val = input()
+root_val = 'A'
+tree = Tree(root_val=root_val)
+
+# number_of_edges = int(input())
+# edge_list = []
+# for i in range(number_of_edges):
+#     s = input()
+#     edge = s.strip().split()
+#     edge_list.append(edge)
+
+edge_list = [('A','B'), ('A','C'), ('A','D'), ('A', 'E'),
+             ('B', 'F'), ('B', 'G'), ('B', 'H'), ('C', 'I'), ('C', 'J'), ('D', 'K'), ('G', 'L'), ('G', 'M'), ('I', 'N'), ('I', 'O'),
+             ('K', 'P'), ('K', 'Q'), ('Q', 'R')]
+
+tree.create_tree(edge_list) 
+tree.print_tree()           
+
+all_nodes, leaf = tree.bfs()
+print("Initial Leaf Nodes:", ",".join([a.value for a in leaf]))
+
+tree.remove(leaf)
+tree.print_tree()
+
+leaf_new, _ = tree.bfs()
+print("New Leaf Nodes:", ",".join([a.value for a in leaf_new]))
